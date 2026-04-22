7099158678805db8e2e496e0c940f2614b2221d1
Andrzej Krzastek <krzasteka@gmail.com>
2026-04-22 15:09:28 -0700

    import existing cloudflare-ddns CT108 into terraform state

---
+++ cloudflare-ddns.tf
@@ -0,0 +1,19 @@
+resource "proxmox_virtual_environment_container" "cloudflare_ddns" {
+  node_name     = "node2"
+  vm_id         = 108
+  unprivileged  = true
+  start_on_boot = true
+
+  initialization {
+    hostname = "cloudflare-ddns"
+  }
+
+  operating_system {
+    type             = "debian"
+    template_file_id = "local:vztmpl/debian-13-standard_13.1-2_amd64.tar.zst"
+  }
+
+  lifecycle {
+    ignore_changes = all
+  }
+}
