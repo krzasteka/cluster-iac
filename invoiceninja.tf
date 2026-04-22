728db14c08d9c9e25d191ae0ad6d83ae472fa7f5
Andrzej Krzastek <krzasteka@gmail.com>
2026-04-22 15:09:22 -0700

    import existing invoiceninja CT112 into terraform state

---
+++ invoiceninja.tf
@@ -0,0 +1,26 @@
+resource "proxmox_virtual_environment_container" "invoiceninja" {
+  node_name     = "node1"
+  vm_id         = 112
+  unprivileged  = true
+  start_on_boot = true
+
+  initialization {
+    hostname = "invoiceninja"
+
+    ip_config {
+      ipv4 {
+        address = "192.168.0.214/24"
+        gateway = "192.168.0.1"
+      }
+    }
+  }
+
+  operating_system {
+    type             = "debian"
+    template_file_id = "local:vztmpl/debian-13-standard_13.1-2_amd64.tar.zst"
+  }
+
+  lifecycle {
+    ignore_changes = all
+  }
+}
