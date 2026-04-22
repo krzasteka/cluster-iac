3ceb8dbd04f3c05431d35aa4d2bc0bd3e5f84035
Andrzej Krzastek <krzasteka@gmail.com>
2026-04-22 15:09:20 -0700

    import existing gemini-bridge CT110 into terraform state

---
+++ gemini-bridge.tf
@@ -0,0 +1,26 @@
+resource "proxmox_virtual_environment_container" "gemini_bridge" {
+  node_name     = "node1"
+  vm_id         = 110
+  unprivileged  = true
+  start_on_boot = true
+
+  initialization {
+    hostname = "gemini-bridge"
+
+    ip_config {
+      ipv4 {
+        address = "192.168.0.212/24"
+        gateway = "192.168.0.1"
+      }
+    }
+  }
+
+  operating_system {
+    type             = "debian"
+    template_file_id = "local:vztmpl/debian-13-standard_13.1-2_amd64.tar.zst"
+  }
+
+  lifecycle {
+    ignore_changes = all
+  }
+}
