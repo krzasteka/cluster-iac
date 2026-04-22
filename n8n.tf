dbd5950ea541449c14b1500b4e2d59ee63a1a0c3
Andrzej Krzastek <krzasteka@gmail.com>
2026-04-22 15:09:18 -0700

    import existing n8n CT109 into terraform state

---
+++ n8n.tf
@@ -0,0 +1,26 @@
+resource "proxmox_virtual_environment_container" "n8n" {
+  node_name     = "node2"
+  vm_id         = 109
+  unprivileged  = true
+  start_on_boot = true
+
+  initialization {
+    hostname = "n8n"
+
+    ip_config {
+      ipv4 {
+        address = "192.168.0.211/24"
+        gateway = "192.168.0.1"
+      }
+    }
+  }
+
+  operating_system {
+    type             = "debian"
+    template_file_id = "local:vztmpl/debian-13-standard_13.1-2_amd64.tar.zst"
+  }
+
+  lifecycle {
+    ignore_changes = all
+  }
+}
